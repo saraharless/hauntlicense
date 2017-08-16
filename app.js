11fbf0314667d45b5d5f0381e753cdb7c07b4f1c
saraharless <sara.e.harless@gmail.com>
2017-08-16 15:02:15 -0500

    validation on first form input

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
 
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator())
 
 
 app.engine('mustache', mustacheExpress());
@@ -16,9 +18,21 @@ app.get('/', function(req, res){
 });
 app.post('/', function(req, res){
   let name = req.body.name;
-  console.log(':' + name + ':');
-  // let html = '<p>Your user name is: </p>' + email;
-
+  req.checkBody('name', 'please provide a name concurrent with ghost guidelines that is between 1 and 10 characters long.').notEmpty();
+  console.log('your username is: ' + name);
+  let errors = req.validationErrors();
+   if (errors) {
+     console.log(errors);
+     res.render('form', {errs:errors});
+   }
+  // let email = req.body.email;
+  // <p>'your email is: ' + email</p>
+  // let death = req.body.death;
+  // console.log('your cause of death was: ' + death);
+  // let haunting = req.body.select;
+  // console.log('your requested license is for: ' + haunting);
+  // let pass = req.body.pass;
+  // console.log('your entrance code is: ' + pass);
   res.render('form')
 });
 app.listen(3000, function(){
